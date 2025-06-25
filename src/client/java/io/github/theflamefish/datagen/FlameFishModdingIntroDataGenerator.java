cd210519053d264509f9b3e32aa7e3e7b45645c7
TheFlameFish <114826427+TheFlameFish@users.noreply.github.com>
2025-06-24 22:49:06 -0700

    Start working on DataGen

---
+++ src/client/java/io/github/theflamefish/datagen/FlameFishModdingIntroDataGenerator.java
@@ -0,0 +1,18 @@
+package io.github.theflamefish.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.data.DataProvider;
+
+public class FlameFishModdingIntroDataGenerator implements DataGeneratorEntrypoint {
+
+  /**
+   * Register {@link DataProvider} with the {@link FabricDataGenerator} during this entrypoint.
+   *
+   * @param fabricDataGenerator The {@link FabricDataGenerator} instance
+   */
+  @Override
+  public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+    FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+  }
+}
